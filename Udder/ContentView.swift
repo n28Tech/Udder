aa9bc7243eff3ae30b7e07e79d32631ed9cd95c9
Kevin Campbell <N28Tech@icloud.com>
2020-11-08 09:46:24 -0500

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Kevin Campbell on 11/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
